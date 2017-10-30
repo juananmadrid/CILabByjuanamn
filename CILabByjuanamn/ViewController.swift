78737086d73f241101df7dc1696b6a5ee66a25f9
Juan Martin Noguera <juan@cloudonmobile.com>
2017-10-30 21:35:59 +0100

    Initial Commit

---
+++ CILabByjuanamn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CILabByjuanamn
+//
+//  Created by Juan Martin Noguera on 30/10/2017.
+//  Copyright © 2017 Juan Martin Noguera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
